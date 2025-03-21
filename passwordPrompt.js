591a9493bd3459f7811b0d11d719678e4dee7582
Shiela N. Llevares <llevaresshiela28@gmail.com>
2025-03-22 04:40:59 +0800

    Add files via upload

--- passwordPrompt.js
+++ passwordPrompt.js
@@ -6,3 +6,8 @@ do {
   }
 } while (password !== "password123");
 console.log("Access granted.");
+
+//https://stackoverflow.com/questions/64454892/looping-prompt-in-javascript-until-condition-is-met
+//https://stackoverflow.com/questions/44472397/javascript-while-loop-with-if-else-if-else-and-counter
+//https://www.w3schools.com/js/js_loop_while.asp
+//https://www.digitalocean.com/community/tutorials/using-while-loops-and-do-while-loops-in-javascript
